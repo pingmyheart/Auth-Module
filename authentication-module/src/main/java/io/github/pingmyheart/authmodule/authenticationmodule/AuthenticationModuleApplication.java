41dc9182cf88090ad8c5056e434967f1cf7fa3ec
GitlabCiCd <antonio.russi.consulting@gmail.com>
2026-04-22 19:57:06 +0200

    [ADD] - init

---
+++ authentication-module/src/main/java/io/github/pingmyheart/authmodule/authenticationmodule/AuthenticationModuleApplication.java
@@ -0,0 +1,13 @@
+package io.github.pingmyheart.authmodule.authenticationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationModuleApplication.class, args);
+    }
+
+}
